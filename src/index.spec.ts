069c06dd9dd16c9ad801bfd76ac35756a30894d7
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-08-23 00:17:54 +0200

    add BanjoChecker class

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,8 @@
+import { BanjoChecker } from './index';
+
 describe('banjo player check', () => {
   it('exists', () => {
+    let banjoChecker = new BanjoChecker();
     expect(banjoChecker).toBeDefined();
   });
 });
