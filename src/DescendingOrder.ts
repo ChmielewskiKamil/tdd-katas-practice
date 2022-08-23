c3f1373658ac44f1b678333b16ccfb5962a4336c
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-08-23 23:58:51 +0200

    refactor: add typescript type to a variable

--- src/DescendingOrder.ts
+++ src/DescendingOrder.ts
@@ -1,7 +1,7 @@
 export class NumberRearanger {
   orderDescending(integer: number): number {
     const numbers: string[] = integer.toString().split('');
-    const result = numbers.sort().reverse().join('');
+    const result: string = numbers.sort().reverse().join('');
     return parseInt(result);
   }
 }
