d21015733b64a6927147af74e1268f4dd82eea42
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-08-23 23:48:00 +0200

    refactor: clean the duplicate code in the test suite

--- src/DescendingOrder.spec.ts
+++ src/DescendingOrder.spec.ts
@@ -1,18 +1,21 @@
 import { NumberRearanger } from './DescendingOrder';
 
+let numberRearanger: NumberRearanger;
+
 describe('Descending order', () => {
+  beforeEach(() => {
+    numberRearanger = new NumberRearanger();
+  });
+
   it('exists', () => {
-    let numberRearanger = new NumberRearanger();
     expect(numberRearanger).toBeDefined();
   });
 
   it("should rearange digits from '42145' to '54421'", () => {
-    let numberRearanger = new NumberRearanger();
     expect(numberRearanger.orderDescending(42145)).toBe(54421);
   });
 
   it("should rearange digits from '145263' to '654321'", () => {
-    let numberRearanger = new NumberRearanger();
     expect(numberRearanger.orderDescending(145263)).toBe(654321);
   });
 });
