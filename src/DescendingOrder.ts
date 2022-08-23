8bb6db2ddb4c95fcf27757d08c154218f2219d08
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-08-23 23:52:44 +0200

    refactor: delete forEach loop in orderDescending
    Parsing each string to an integer was unnecessary, because
    it is possible to sort the string itself.

--- src/DescendingOrder.ts
+++ src/DescendingOrder.ts
@@ -1,9 +1,6 @@
 export class NumberRearanger {
   orderDescending(integer: number): number {
     const numbers: string[] = integer.toString().split('');
-    numbers.forEach((number) => {
-      parseInt(number);
-    });
     const result = numbers.sort().reverse().join('');
     return parseInt(result);
   }
