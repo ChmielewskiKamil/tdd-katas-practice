cc2373acb69fc79203e90c1753d6d5872824f529
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-08-23 17:26:23 +0200

    refactor test suite with beforeEach method

--- src/AreYouPlayingBanjo.spec.ts
+++ src/AreYouPlayingBanjo.spec.ts
@@ -1,18 +1,20 @@
 import { BanjoChecker } from './AreYouPlayingBanjo';
 
+let banjoChecker: BanjoChecker;
+beforeEach(() => {
+  banjoChecker = new BanjoChecker();
+});
+
 describe('banjo player check', () => {
   it('exists', () => {
-    let banjoChecker = new BanjoChecker();
     expect(banjoChecker).toBeDefined();
   });
 
   it("should be able to tell that 'rob' plays banjo", () => {
-    let banjoChecker = new BanjoChecker();
     expect(banjoChecker.checkIfPlayingBanjo('rob')).toBeTruthy();
   });
 
   it("should be able to tell that 'bob' doesn't play banjo", () => {
-    let banjoChecker = new BanjoChecker();
     expect(banjoChecker.checkIfPlayingBanjo('bob')).toBeFalsy();
   });
 });
