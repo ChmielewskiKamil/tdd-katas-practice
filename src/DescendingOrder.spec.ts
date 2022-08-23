729e3028be682ad2dc7ed9512b014bbe042333d3
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-08-23 23:28:34 +0200

    test: add test for orderDescending method with different values

--- src/DescendingOrder.spec.ts
+++ src/DescendingOrder.spec.ts
@@ -10,4 +10,9 @@ describe('Descending order', () => {
     let numberRearanger = new NumberRearanger();
     expect(numberRearanger.orderDescending(42145)).toBe(54421);
   });
+
+  it("should rearange digits from '145263' to '654321'", () => {
+    let numberRearanger = new NumberRearanger();
+    expect(numberRearanger.orderDescending(145263)).toBe(654321);
+  });
 });
