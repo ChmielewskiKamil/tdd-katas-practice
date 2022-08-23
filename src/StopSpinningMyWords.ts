e22a51beefbac88af1b1749dd0ceb47bb3ecea00
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-08-23 18:23:57 +0200

    refactor WordSpiner class with reverseWord method

--- src/StopSpinningMyWords.ts
+++ src/StopSpinningMyWords.ts
@@ -1,7 +1,11 @@
 import { stringify } from 'querystring';
 
 export class WordSpiner {
+  reverseWord(str: string): string {
+    return str.split('').reverse().join('');
+  }
+
   spinWords(str: string): string {
-    return str.length > 4 ? str.split('').reverse().join('') : str;
+    return str.length > 4 ? this.reverseWord(str) : str;
   }
 }
