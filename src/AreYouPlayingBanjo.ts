9f19d97e951a4718e3ac2afefb8248008f918bb5
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-08-23 17:31:50 +0200

    fix the different casing failing

--- src/AreYouPlayingBanjo.ts
+++ src/AreYouPlayingBanjo.ts
@@ -1,5 +1,5 @@
 export class BanjoChecker {
   checkIfPlayingBanjo(name: string): boolean {
-    return name[0] == 'r' ? true : false;
+    return name[0] === 'r' || name[0] === 'R' ? true : false;
   }
 }
