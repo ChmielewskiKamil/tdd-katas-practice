0feb510699a195015723664efa431b5435ce1fc4
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-08-24 11:47:08 +0200

    fix: change logic to add whitespaces

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,28 @@ export class TowerBuilder {
       starTower.push('*');
       // we need to start at 0, because star tower array is indexed from 0
       // because of that we need to subtract 1 from the number of floors
+      let spacesToAdd: string = this.addSpacesToEachSide(numberOfFloors - 1);
+      starTower[0] = spacesToAdd + '*' + spacesToAdd;
+
       for (let floor = 0; floor < numberOfFloors - 1; floor++) {
-        starTower.push(starTower[floor] + '**');
+        spacesToAdd = this.addSpacesToEachSide(numberOfFloors - 2 - floor);
+        starTower.push(
+          spacesToAdd +
+            starTower[floor].trimEnd().trimStart() +
+            '**' +
+            spacesToAdd,
+        );
       }
       return starTower;
     } else {
       return [''];
     }
+  }
+  addSpacesToEachSide(amountOfSpacesToAdd: number): string {
+    let spacesToAdd = '';
+    for (let space = 0; space < amountOfSpacesToAdd; space++) {
+      spacesToAdd += ' ';
+    }
+    return spacesToAdd;
   }
 }
