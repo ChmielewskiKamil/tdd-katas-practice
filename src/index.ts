41f875fe9c8197c32756aacc42da779081d0217f
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-08-24 10:13:06 +0200

    fix: add logic to work for 2 floor tower

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 export class TowerBuilder {
   buildTower(numberOfFloors: number): string[] {
-    return ['*'];
+    let starTower: string[] = [];
+
+    if (numberOfFloors === 1) {
+      return ['*'];
+    } else if (numberOfFloors > 1) {
+      starTower.push('*');
+      let starsToAppend: string = '';
+      for (let floor = 0; floor < numberOfFloors + 1; floor++) {
+        starsToAppend += '*';
+      }
+      starTower.push(starsToAppend);
+      return starTower;
+    } else {
+      return [''];
+    }
   }
 }
