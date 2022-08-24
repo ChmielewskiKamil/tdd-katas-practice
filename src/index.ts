13f5909afd80564067f43d1221f585e31382a1da
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-08-24 10:49:48 +0200

    refactor: remove console.logs used in development

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,10 @@ export class TowerBuilder {
       return ['*'];
     } else if (numberOfFloors > 1) {
       starTower.push('*');
-      console.log(starTower);
       // we need to start at 0, because star tower array is indexed from 0
       // because of that we need to subtract 1 from the number of floors
       for (let floor = 0; floor < numberOfFloors - 1; floor++) {
-        console.log(`Star tower before adding: ${starTower}`);
         starTower.push(starTower[floor] + '**');
-        console.log(`Star tower after adding: ${starTower}`);
       }
       return starTower;
     } else {
