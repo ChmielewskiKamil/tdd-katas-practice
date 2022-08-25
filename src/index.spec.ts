b5ed820da33f011d81ce18aa05150ec905c81ba7
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-08-25 11:44:46 +0200

    test: add test for rotation of [1,2] by 1

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -9,4 +9,8 @@ describe('Rotate array', () => {
     let arrayRotator = new ArrayRotator();
     expect(arrayRotator.rotateLeft(1, [1])).toStrictEqual([1]);
   });
+  it('should rotate [1,2] to [2,1] for 1 rotation', () => {
+    let arrayRotator = new ArrayRotator();
+    expect(arrayRotator.rotateLeft(1, [1, 2])).toStrictEqual([2, 1]);
+  });
 });
