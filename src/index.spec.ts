11c9df9675ec75e5d80c0a34b8061dc055285fb4
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-08-25 11:31:45 +0200

    test: add test for left rotate of [1] by 1

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,4 +5,8 @@ describe('Rotate array', () => {
     let arrayRotator = new ArrayRotator();
     expect(arrayRotator).toBeDefined();
   });
+  it('should rotate [1] to [1]', () => {
+    let arrayRotator = new ArrayRotator();
+    expect(arrayRotator.rotateLeft(1, 1, 1)).toBe([1]);
+  });
 });
