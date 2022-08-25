2eb35da1f1e9f24f15d5ff85788ed3b9dcbb55f1
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-08-25 12:02:11 +0200

    test: refactor tests - remove duplicate code

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,16 +1,19 @@
 import { ArrayRotator } from './index';
 
+let arrayRotator: ArrayRotator;
+
 describe('Rotate array', () => {
+  beforeEach(() => {
+    arrayRotator = new ArrayRotator();
+  });
+
   it('exists', () => {
-    let arrayRotator = new ArrayRotator();
     expect(arrayRotator).toBeDefined();
   });
   it('should rotate [1] to [1]', () => {
-    let arrayRotator = new ArrayRotator();
     expect(arrayRotator.rotateLeft(1, [1])).toStrictEqual([1]);
   });
   it('should rotate [1,2] to [2,1] for 1 rotation', () => {
-    let arrayRotator = new ArrayRotator();
     expect(arrayRotator.rotateLeft(1, [1, 2])).toStrictEqual([2, 1]);
   });
 });
