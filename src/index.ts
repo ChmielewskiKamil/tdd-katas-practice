2618daf7410a48ce293499c42e4511973d366881
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-08-25 11:59:56 +0200

    fix: add logic to rotate elements

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 export class ArrayRotator {
   rotateLeft(numberOfRotations: number, elements: number[]): number[] {
+    let elementToMove = elements.shift();
+    if (elementToMove !== undefined) {
+      elements.push(elementToMove);
+      return elements;
+    }
     return elements;
   }
 }
